6c1b0b4da18960ab266e19d43c51dea0699874d0
yegonke <49680358+yegonke@users.noreply.github.com>
2023-01-04 15:59:38 +0300

    Update settings.py

--- mobileotp/mobileotp/settings.py
+++ mobileotp/mobileotp/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&v5-10vs(*x8*0)z#47=e96z@8^eyo43c54w84euk9lhqb4n^^'
+SECRET_KEY = 'my-secret'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
